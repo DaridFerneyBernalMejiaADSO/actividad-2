558e39298599833c5aced511ef284f34bc66382d
DaridFerneyBernalMejiaADSO <daridbernal2004@gmail.com>
2022-10-30 18:16:18 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,30 @@
+addEventListener("DOMContentLoaded", (e)=>{
+    
+        let hora = 5.208
+        var objeto=document.getElementById('id1');
+        console.log(objeto*hora)
+        
+       /* let total=hora*obreros
+        console.log(total)*/
+   /*for (i in Range(1,50,)){*/
+       /*- let obrar                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        = document.querySelector("#obrar");
+        obrar.addEventListener("submit", (e)=>{
+            e.preventDefault();
+            let datainput = Object.fromEntries(new FormData(e.target));
+            console.log(total);*/
+           /* let myinput = document.querySelector(`input[name="obreros"]`);
+            myinput.placeholder="ingrese horas";
+            alert.log*/
+      /*  })*/
+        
+
+
+
+   /* }*/
+
+
+
+ })
+
+    
+ 
